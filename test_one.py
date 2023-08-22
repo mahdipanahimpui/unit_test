019446f5ea80cbf7c8d666b84744b5127cbe4f44
mahdi <mahdipanahimpui@gmail.com>
2023-08-22 10:57:52 +0330

    raise_in_pytest

--- test_one.py
+++ test_one.py
@@ -1,29 +1,17 @@
-import unittest
+import pytest
 import one
 
 
 
-# the class name is <file_to_test>Test
-class OneTest(unittest.TestCase):
-    # test_<def that is going to test>
+class TestOne:
     def test_division(self):
-        self.assertEqual(one.devision(-1, 4), -.25)
-        # for devisions:
-        self.assertRaises(ZeroDivisionError, one.devision, 4, 0)
-
+        assert one.division(10,2) == 5
+        with pytest.raises(ZeroDivisionError):
+            one.division(3, 0)
 
 
     def test_add(self):
-        self.assertEqual(one.add(5,0), 5)    
-        self.assertEqual(one.add(5,2), 7)  
+        assert one.add(10, 2) == 12
 
 
-
-
-if __name__ == '__name__':
-    unittest.main()
-    # run in terminal:
-    # python -m unittest test_one.py
-
-# to run all tests file:
-# python -m unittest discover
+# pytest test_one.py --resultlog=result.log
