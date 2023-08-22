e0bae84f60ac3bd27ec2d4af55842bb7b5cc9cad
mahdi <mahdipanahimpui@gmail.com>
2023-08-22 10:29:02 +0330

    pytest_mixtures

--- setup_tearown/test_person.py
+++ setup_tearown/test_person.py
@@ -1,35 +1,27 @@
-import unittest
 from person import Person
+import pytest
 
+# teardown and setup in pytest
 
-class PersonTest(unittest.TestCase):
-    # setup runs befor mehtods, 
-    # tearDown runs after mehtods,
-
-    # both above <>class with @classmethod runs befor and after class
-
-
-
-    def setUp(self):
-        self.p1 = Person('mahdi', 'panahi')
-        self.p2 = Person('mehran', 'panahi')
-
-    
-    def tearDown(self):
+# start of class should be Test
+class TestPerson:
+    @pytest.fixture
+    def setup(self):
+        self.p1 = Person('amir', 'big')
+        self.p2 = Person('john', 'doe')
+ 
+        # for teardown use yield '<method name>'
+        yield 'setup'
         print('Done.')
 
 
-
-    def test_fullname(self):
-        self.assertEqual(self.p1.fullname(), 'mahdi panahi')
-        self.assertEqual(self.p2.fullname(), 'mehran panahi')
-
-
-    def test_email(self):
-        self.assertEqual(self.p1.email(), 'mahdipanahi@email.com')
-        self.assertEqual(self.p2.email(), 'mehranpanahi@email.com')
+    def test_fullname(self, setup):
+        assert self.p1.fullname() == 'amir big'
+        assert self.p2.fullname() == 'john doe'
 
 
+    def test_email(self, setup):
+        assert self.p1.email() == 'amirbig@email.com'
+        assert self.p2.email() == 'johndoe@email.com'
 
-if __name__ == "__name__":
-    unittest.main()
+
